ac407de665d3584e8bc11529f3999a6fa9caf6dd
sokkhear8712 <129651152+sokkhear8712@users.noreply.github.com>
2023-04-18 13:52:17 +0300

    Create average_speed.py

---
+++ average_speed.py
@@ -0,0 +1,6 @@
+distance = float(input("Введите расстояние в километрах: "))
+time = float(input("Введите время в часах: "))
+
+average_speed = distance / time
+
+print("Средняя скорость:", average_speed, "км/ч")
